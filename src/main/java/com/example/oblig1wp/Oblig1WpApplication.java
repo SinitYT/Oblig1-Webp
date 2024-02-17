78b172c98688d28878d49d2bcbc0ea9d4f1780a3
Sinit <sinityonas@outlook.com>
2024-02-17 22:27:34 +0100

    Opprettelse av Spring boot project

---
+++ src/main/java/com/example/oblig1wp/Oblig1WpApplication.java
@@ -0,0 +1,13 @@
+package com.example.oblig1wp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig1WpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig1WpApplication.class, args);
+    }
+
+}
